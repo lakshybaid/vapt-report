77edf529f9dd11c8f9ea4ac181d7aeadd3c2f7b0
LAKSHY BAID <lakshy.baid21@gmail.com>
2026-03-12 11:45:53 +0530

    Update app.run to use environment variable for port

--- backend.py
+++ backend.py
@@ -1187,4 +1187,5 @@ def reset_password(uid):
 
 # ══════════════════════════════════════════════════════════════════════════
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
